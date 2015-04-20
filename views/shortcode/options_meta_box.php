dd1c6ca45e664e968f513513af518ded50a8d47b
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-20 16:16:28 +0000

    before updating removing old files
    git-svn-id: https://plugins.svn.wordpress.org/wp-meta-data-filter-and-taxonomy-filter/trunk@1139263 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/shortcode/options_meta_box.php
+++
@@ -1,3 +0,0 @@
-<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
-
-In the premium version only! You can do by shortcodes search-forms like: <a href="http://cars.wordpress-filter.com/" class="button" target="_blank">Car Dealer</a><br />
